6fd1212d03083ad0c12302eb381bc4ee35676719
kylqin <kylqin@outlook.com>
2021-05-17 23:23:18 +0800

    使用 websockets

---
+++ src/events.gateway.ts
@@ -0,0 +1,37 @@
+import { SubscribeMessage, WebSocketGateway, WsResponse, WebSocketServer } from '@nestjs/websockets';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators'
+const l = console.log
+
+@WebSocketGateway(3002)
+export class EventsGateway {
+  @WebSocketServer() server;
+
+  @SubscribeMessage('events')
+  onEvent(client: any, payload: any): Observable<WsResponse<any>> | any {
+    l(client);
+    // this.server.emit('resmsg', data);  // io.emit('resmsg', payload)
+    let { name } = payload;
+    if (name === 'ajanuw') {
+      return of({
+        event: 'events',
+        data: {
+          msg: 'hello ajanuw!'
+        }
+      })
+    }
+    if (name === 'alone') {
+      return of('hi', '实打实')
+        .pipe(
+          map($_ =>
+            ({
+              event: 'events', data: {
+                msg: $_
+              }
+            }))
+        );
+    }
+    return of(payload);
+  }
+
+}
